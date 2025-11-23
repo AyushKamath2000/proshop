4dfbdcb0615e0d32dc99966f9218a755c0a5cd04
Akamath2000 <127821525+Akamath2000@users.noreply.github.com>
2025-11-23 16:55:14 +0530

    remove unwanted changes

--- backend/models/wishlistModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const wishlistItemSchema = mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-  variant: { type: mongoose.Schema.Types.ObjectId, ref: 'ProductVariant' },
-  addedAt: { type: Date, default: Date.now }
-}, { timestamps: true });
-
